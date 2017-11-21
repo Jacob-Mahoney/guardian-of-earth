e5837770d8fdca8dbc27757eaeee84f13a47248b
jacobmahoney <mahoney.jacob@gmail.com>
2017-11-20 20:34:27 -0600

    Added meteors list to ParticleHandler
    Did this to increase performance because checkCollisions now has to only loop through all the meteors instead of all the particles (and check if each particle is a meteor)

--- app/src/main/java/jacobmahoney/guardianofearth/ParticleHandler.java
+++ app/src/main/java/jacobmahoney/guardianofearth/ParticleHandler.java
@@ -13,6 +13,7 @@ import java.util.concurrent.CopyOnWriteArrayList;
 public class ParticleHandler extends Observable implements UpdateableGameObject, DrawableGameObject, Observer {
 
     private List<Particle> particles;
+    private List<Meteor> meteors;
     private Paint paint;
 
     public ParticleHandler() {
@@ -22,11 +23,15 @@ public class ParticleHandler extends Observable implements UpdateableGameObject,
         paint.setStyle(Paint.Style.FILL);
 
         particles = new CopyOnWriteArrayList<>();
+        meteors = new CopyOnWriteArrayList<>();
 
     }
 
     public void addParticle(Particle p) {
         particles.add(p);
+        if (p instanceof Meteor) { // if particle is a meteor, also add it to meteors list
+            meteors.add((Meteor) p);
+        }
     }
 
     // make function for getting the last meteor destroyed or the position of it
@@ -36,18 +41,16 @@ public class ParticleHandler extends Observable implements UpdateableGameObject,
     // also could make function for getting the last meteor that hit earth
     // for the same reason
 
-    private Particle checkCollisions(Particle laser) {
+    private Meteor checkCollisions(Particle laser) {
 
-        Iterator<Particle> iter = particles.iterator();
-        Particle p;
-        while (iter.hasNext()) { // looping through all particles
-            p = iter.next();
-            if (p instanceof Meteor) { // if particle is a meteor, then see if it collides with the passed in laser
-                if (p.intersect(laser)) {
-                    setChanged();
-                    notifyObservers(GameController.Event.METEOR_DESTROYED);
-                    return p;
-                }
+        Iterator<Meteor> iter = meteors.iterator();
+        Meteor m;
+        while (iter.hasNext()) { // looping through all meteors
+            m = iter.next();
+            if (m.intersect(laser)) {
+                setChanged();
+                notifyObservers(GameController.Event.METEOR_DESTROYED);
+                return m;
             }
         }
 
@@ -59,8 +62,7 @@ public class ParticleHandler extends Observable implements UpdateableGameObject,
     public void update(int screenWidth, int screenHeight) {
 
         List<Particle> particlesToRemove = new LinkedList<>();
-
-        int meteors = 0;
+        List<Meteor> meteorsToRemove = new LinkedList<>();
 
         Iterator<Particle> iter = particles.iterator();
         Particle p;
@@ -68,26 +70,28 @@ public class ParticleHandler extends Observable implements UpdateableGameObject,
             p = iter.next();
             p.update();
             if (p instanceof Laser) {
-                Particle m = checkCollisions(p); // returns the collided-with meteor if there was a collision, null if not
+                Meteor m = checkCollisions(p); // returns the collided-with meteor if there was a collision, null if not
                 if (m != null) {
                     particlesToRemove.add(p);
                     particlesToRemove.add(m);
+                    meteorsToRemove.add(m);
                 } else if (p.offscreen(screenWidth, screenHeight)) { // no collision with any meteors... still need to check if offscreen
                     particlesToRemove.add(p);
                 }
             } else {
-                meteors++;
                 if (p.offscreen(screenWidth, screenHeight)) { // if meteor went off screen, it meant it hit the earth, so need to alert GameController
                     setChanged();
                     notifyObservers(GameController.Event.METEOR_HIT_EARTH);
                     particlesToRemove.add(p);
+                    meteorsToRemove.add((Meteor) p);
                 }
             }
         }
 
         particles.removeAll(particlesToRemove);
+        meteors.removeAll(meteorsToRemove);
 
-        if (meteors == 0) {
+        if (meteors.size() == 0) {
             setChanged();
             notifyObservers(GameController.Event.NO_METEORS_ON_SCREEN);
         }
